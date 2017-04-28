ab1f7ad1210cc5ded5c273d1685d0b906134c19a
Hank V <hhveach@yahoo.com>
2017-04-27 23:26:25 -0400

    signin

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ $(document).ready(function(){
     mobileFirst: true
   });
 });
+
+let livePopup = document.querySelector('.live');
+let hov = document.querySelector('.popup');
+  hov.addEventListener('mouseenter', function(){
+    livePopup.style.display = 'block';
+    });
+    hov.addEventListener('mouseleave', function(){
+      livePopup.style.display = 'none';
+      });
