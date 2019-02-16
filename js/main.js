0d4a671e15b32564678c6d2acd24c79e67964dbd
Hank V <hhveach@yahoo.com>
2019-02-15 23:34:34 -0500

    remove comments

--- js/main.js
+++ js/main.js
@@ -6,19 +6,6 @@ $(document).ready(function(){
     mobileFirst: true
   });
 });
-// let livePopup = document.querySelector('.live');
-// let hov = document.querySelector('.popup');
-//   hov.addEventListener('mouseenter', function(){
-//     livePopup.style.display = 'block';
-//     });
-//     hov.addEventListener('mouseleave', function(){
-//       livePopup.style.display = 'none';
-//       });
-
-// let navs = document.querySelectorAll('.slow');
-// let navArray = [];
-// navArray.push(...navs);
-// navArray.map((els) => els.addEventListener('click', () => console.log('working')));
 
 $('a[href*=\\#]').on('click', function(event){
     event.preventDefault();
